aa406ff61d5f074153740c574ec18542d52e0190
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-11 11:18:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ h1.style.color= 'blue';
 
 let btn = document.querySelector('button');
 btn.onclick=()=>{
-  document.body.style.backgroundColor='red';
+  let color = '#' + (1000000 + Math.floor(Math.random()*1000000)).toString(15);
+  document.body.style.backgroundColor= color;
 }

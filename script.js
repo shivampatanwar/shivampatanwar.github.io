3944291754b11b675f1702b667b4b4d7143f919d
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-12 02:20:53 +0530

    a

--- script.js
+++ script.js
@@ -7,14 +7,27 @@
 
 // }
 
-console.log(document)
 
 if (document.querySelector('#random-bg') != null) {
   let btn = document.querySelector('#random-bg');
-  console.log(btn)
   btn.onclick = () => {
-    let color = '#' + (1000000 + Math.floor(Math.random() * 1000000)).toString(15);
+    let min=1000000;
+    let max = 10000000;
+    let color = '#' + (Math.floor(Math.random()*(max-min)+min)).toString(16);
+    console.log(color)
     document.body.style.backgroundColor = color;
+  }
+}
+
+if (document.querySelector('#random') != null) {
+  let btn = document.querySelector('#random');
+  btn.onclick = () => {
+    // let random = 100000 + Math.floor(Math.random() * 1000000);
+    let min=100000;
+    let max = 1000000;
+    let random = Math.floor(Math.random()*(max-min)+min);
+    console.log(random);
+    
 
   }
 }

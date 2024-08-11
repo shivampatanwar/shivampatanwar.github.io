d4b914e6449e7e2c66168e0921604ea57b25e66e
Shivam Patanwar <shivampatanwarofficial@gmail.com>
2024-08-12 02:21:08 +0530

    a

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 
-// let btn = document.querySelector('button');
-// console.log(btn)
-// btn.onclick=()=>{
-//   let color = '#' + (1000000 + Math.floor(Math.random()*1000000)).toString(15);
-//   document.body.style.backgroundColor= color;
-
-// }
-
 
 if (document.querySelector('#random-bg') != null) {
   let btn = document.querySelector('#random-bg');
